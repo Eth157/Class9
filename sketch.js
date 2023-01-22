2d17aef8ec35c79eadb53d36b5a9c5b5097fbf52
Eth157 <123294735+Eth157@users.noreply.github.com>
2023-01-22 20:14:57 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  
+  createCanvas(400,400);
+  box=createSprite(200,200,10,10);
+  box.shapeColor="red"
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
